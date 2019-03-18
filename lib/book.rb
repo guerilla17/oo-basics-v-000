2e85345eb1f05f390c50d41cb7a9bc21f8bd117f
Gigi M <mmgamao1@gmail.com>
2019-03-18 18:35:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -41,4 +41,31 @@ end
  
  
 class Shoe
-  def initialize()
+  def initialize(brand)
+    @brand = brand
+  end
+  def brand 
+    @brand
+  end
+  def color = (color)
+    @color = color
+  end
+  def color
+    @color
+  end
+  def size = (size)
+    @size = size
+  end
+  def material = (material)
+    @material = material
+  end
+  def condition = (condition)
+    @condition = condition
+  end
+  def condition
+    @condition
+    def cobble(shoe)
+      puts "The shoe has been repaired!"
+      puts "The shoe's condition is new!"
+    end
+    
