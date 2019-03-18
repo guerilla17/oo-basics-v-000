ed2deef398a0486a6d35b81d6d296aee81ccb24c
Gigi M <mmgamao1@gmail.com>
2019-03-18 18:36:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -41,31 +41,15 @@ end
  
  
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
   def initialize(brand)
     @brand = brand
   end
-  def brand 
-    @brand
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
   end
-  def color = (color)
-    @color = color
-  end
-  def color
-    @color
-  end
-  def size = (size)
-    @size = size
-  end
-  def material = (material)
-    @material = material
-  end
-  def condition = (condition)
-    @condition = condition
-  end
-  def condition
-    @condition
-    def cobble(shoe)
-      puts "The shoe has been repaired!"
-      puts "The shoe's condition is new!"
-    end
-  end 
+end
